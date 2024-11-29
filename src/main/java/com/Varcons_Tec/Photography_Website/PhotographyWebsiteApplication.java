a6cf9686e7f2f942221ebbf3e636aa0971386e94
Harsha C S <107195246+harshavarden@users.noreply.github.com>
2024-11-29 16:08:24 +0530

    Add files via upload

---
+++ src/main/java/com/Varcons_Tec/Photography_Website/PhotographyWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.Varcons_Tec.Photography_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotographyWebsiteApplication.class, args);
+		System.out.println("Here is where it all starts...!");
+	}
+
+}
